67015d83012d50ac073037268ab4008f1603f50f
josuepl <josue.perez.lucero@gmail.com>
2022-01-25 11:48:11 -0600

    Update intro.py

--- intro.py
+++ intro.py
@@ -3,23 +3,29 @@
 Este es un comentario multilinea
 Es decir se escriben varias lineas
 '''
+
+
 class Persona:
     def __init__(self,nombre,edad,lista) -> None:
         self.nombre = nombre
         self.edad = edad
         self.lista = lista
         pass
-
     def __repr__(self) -> str:
         return "nombre: " + self.nombre + " - edad: "+ str(self.edad)+ " - lista: " + str(self.lista)
         pass
 
     def funcion(self):
-        
+
         pass
     pass
 
-
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, lista, grado, calif) -> None:
+        super().__init__(nombre, edad, lista)
+        self.grado = grado
+        self.calif = calif
+    pass
 
 # Datos primitivos
 a = 1
@@ -40,5 +46,7 @@ print(a," - ",type(a))
 a1 = {'nombre':'Josue','edad':45, 'lista':a}
 print(a1," - ",type(a1))
 # Datos Abstractos
-a = Persona('Josue',45,a)
-print(a," - ",type(a))
+a2 = Persona('Josue',45,a)
+print(a2," - ",type(a2))
+b = Estudiante('Fulanito', 20,a,"primer",10)
+print(b," - ",type(b))
