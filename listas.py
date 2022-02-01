681256505f0df692d65bc7e05acc535265f46a36
josuepl <josue.perez.lucero@gmail.com>
2022-02-01 11:40:48 -0600

    Update listas.py

--- listas.py
+++ listas.py
@@ -19,6 +19,37 @@ class Nodo:
         pass
     pass
 
+def agregaMascota():
+    print("Cual es el nombre de tu mascota: ")
+    nombre = input()
+    print("Cual es la raza de tu mascota: ")
+    raza = input()
+    print("Cual es la edad de tu mascota: ")
+    edad = int(input())
+    return Nodo(nombre,raza,edad)
+    pass
+
+def agregaNodo(lista,nodo):
+    if lista.__len__() == 0:
+        print("Lista Vacia \n")
+        lista = nodo
+    else:
+        for elemento in lista:
+            if elemento.siguiente != None:
+                elemento.siguiente = nodo
+        pass
+    pass
+
+def imprimeLista(lista):
+    if lista.__len__() == 0:
+        print("Lista Vacia")
+        print("[ ]")
+    else:
+        for elemento in lista:
+            print(elemento," \n")
+    pass
+
+
 def menu():
     # Menu principal
     menuTxt = "------------ Lista de Mascotas ------------------- \n"
@@ -27,17 +58,23 @@ def menu():
     menuTxt+="3) Ver Lista de Mascotas \n"
     menuTxt+="4) Salir \n"
     menuTxt+="Elije la opcion deseada: \n"
-    print(menuTxt)
-    opcion = input()
-    if opcion == '1':
-        print("Has elegido la opcion de agregar: \n")
-    if opcion == '2':
-        print("Has elegido la opcion de Eliminar: \n")
-    if opcion == '3':
-        print("Has elegido la opcion de Ver: \n")
-    if opcion == '4':
-        print("Has elegido la opcion de Salir: \n")
-        os._exit(4)
+    opcion = "Inicial1"
+    listaM = []
+    while opcion != '4':
+        print(menuTxt)
+        opcion = input()
+        if opcion == '1':
+            print("Has elegido la opcion de agregar: \n")
+            mascotaN = agregaMascota()
+            agregaNodo(listaM,mascotaN)
+        if opcion == '2':
+            print("Has elegido la opcion de Eliminar: \n")
+        if opcion == '3':
+            print("Has elegido la opcion de Ver: \n")
+            imprimeLista(listaM)
+        if opcion == '4':
+            print("Has elegido la opcion de Salir: \n")
+            os._exit(4)
     pass
 
 if __name__ == "__main__":
