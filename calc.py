70000f054e46559c59a3784091f9d5b1bf03e0a0
josuepl <josue.perez.lucero@gmail.com>
2022-03-29 11:27:32 -0600

    Update calc.py

--- calc.py
+++ calc.py
@@ -7,6 +7,7 @@ from pyparsing import col
 root = Tk()
 root.title("Calculadora básica")
 root.geometry("300x400")
+root.resizable(width=False, height=False)
 styleF = ttk.Style()
 styleF.configure('principal.TFrame',background = '#0059b3', borderwith = 5, relief='sunken')
 marcoPrincipal = ttk.Frame(root,style="principal.TFrame",width=300, height=400).grid(column=0 , row=0,sticky=(N,W,E,S),padx=(5,5), pady=(2,2)) # N Norte - Superior, W - West - Oeste - Izquierdo
